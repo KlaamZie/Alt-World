834a7d0ece5873497500141b514dd8b359502735
Enzo Viry <enzo.viry@gmail.com>
2023-07-03 15:51:09 +0200

    fix: refactor code

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
-
 export default {
     root: 'src/',
     publicDir: '../static/',
@@ -7,7 +5,7 @@ export default {
     server:
     {
         host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
+        open: true // Open if it's not a CodeSandbox
     },
     build:
     {
